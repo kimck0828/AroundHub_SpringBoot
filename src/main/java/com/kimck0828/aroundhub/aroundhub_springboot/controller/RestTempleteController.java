df184fbd62356d5b09f735be7f7ec1f43d9d27d9
kimck0828 <kimck0828@gmail.com>
2022-06-17 17:49:34 +0900

    ShortUrl 実験

--- src/main/java/com/kimck0828/aroundhub/aroundhub_springboot/controller/RestTempleteController.java
+++ src/main/java/com/kimck0828/aroundhub/aroundhub_springboot/controller/RestTempleteController.java
@@ -2,10 +2,18 @@ package com.kimck0828.aroundhub.aroundhub_springboot.controller;
 
 import com.kimck0828.aroundhub.aroundhub_springboot.dto.MemberDto;
 import com.kimck0828.aroundhub.aroundhub_springboot.service.RestTempleteService;
+import java.net.URI;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("api/rest-templete")
@@ -38,5 +46,33 @@ public class RestTempleteController {
   @PostMapping("/add-header")
   public ResponseEntity<MemberDto> addHeader() {
     return service.addHeader();
+  }
+  
+  @GetMapping("/short-url")
+  public String shortUrl(@RequestParam String url) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("X-RapidAPI-Key", "164f928ba5mshf23a1495c0c62a1p106b49jsnb6ada11dd2ca");
+    headers.add("X-RapidAPI-Host", "url-shortener-service.p.rapidapi.com");
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    
+    RestTemplate restTemplate = new RestTemplate();
+    RequestEntity<ShortUrlDto> requestEntity = RequestEntity
+        .post("https://url-shortener-service.p.rapidapi.com/shorten")
+        .headers(headers)
+        .body(new ShortUrlDto(url));
+    ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity,
+        String.class);
+    return responseEntity.getBody();
+  }
+  
+  @Data
+  @AllArgsConstructor
+  class ShortUrlDto {
+    String url;
+  }
+  @Data
+  @AllArgsConstructor
+  class ShortUrlResponse{
+    String resultUrl;
   }
 }
